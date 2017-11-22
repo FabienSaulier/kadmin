e0f3c0f7a6fa2e24867e9ea7549173d5ec10dc16
Fabien Saulier <fsaulier@gmail.com>
2017-11-22 01:40:27 +0100

    z

--- server.js
+++ server.js
@@ -4,5 +4,6 @@ const serveStatic = require('serve-static')
 const app = express()
 
 app.use(serveStatic(path.join(__dirname, 'dist')));
+console.log(process.env.PORT);
 const port = process.env.PORT || 5000
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3000, () => console.log('Example app listening on port :'+port))
