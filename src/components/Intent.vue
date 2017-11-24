275ee065ade380e36130d7425eaef4eeee09633a
Fabien Saulier <fsaulier@gmail.com>
2017-11-24 21:17:55 +0100

    plug to kanzapi

--- src/components/Intent.vue
+++ src/components/Intent.vue
@@ -5,16 +5,39 @@
 </template>
 
 <script>
+import  * as Kanzapi from '../lib/kanzapi'
+import axios from 'axios';
+
 export default {
   name: 'Intent',
 
   created(){
     console.log("load data ", this.species, this.intent);
+    // this.intentsList = Kanzapi.getIntents(this.species, this.intent);
+    // TODO pb: promise ??
+    const url = 'http://localhost:3000/species/'+this.species+'/intent/'+this.intent;
+    console.log(url);
+    axios.get(url)
+      .then((response) => {
+        const data = response.data;
+        this.intentsList = data;
+        console.log(data);
+        /*
+        this.answer = Object.assign({}, this.answer, data); // answer is reactive
+        this.originalAnswer = JSON.parse(JSON.stringify(data));
+        // data.answer come up with observer ??? wtf ????
+*/
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
   },
   data() {
     return {
       species: this.$route.params.species,
       intent: this.$route.params.intent,
+      intentsList: {},
     };
   },
 
