436f221a562eb31c6d19329a4ee57cade85d4d56
Fabien Saulier <fsaulier@gmail.com>
2017-11-27 19:12:07 +0100

    answer card list

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@
                 </v-list-tile-title>
               </v-list-tile-content>
             </v-list-tile>
-            <v-list-tile v-for="(child, i) in item.children" :key="i" @click="clickIntent(item.text, child.text)">
+            <v-list-tile  v-for="(child, i) in item.children" :key="i" @click="clickIntent(item.text, child.text)">
               <v-list-tile-action v-if="child.icon">
                 <v-icon>{{ child.icon }}</v-icon>
               </v-list-tile-action>
--- src/components/Intent.vue
+++ src/components/Intent.vue
@@ -2,16 +2,84 @@
   <div style="width:800px">
     <h3>Intent {{this.intent}}</h3>
     <br  />
-    <div  v-for="item in intentsList">
+    <div  v-for="answer in answersList">
 
       <v-expansion-panel>
         <v-expansion-panel-content >
-          <div slot="header">{{item.name}} - {{item.description}}</div>
+          <div slot="header">{{answer.name}} - {{answer.description}}</div>
           <v-card>
-            <v-card-text class="grey lighten-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            </v-card-text>
+
+            <v-card-text>
+
+      <v-container fluid>
+
+        <v-layout row>
+          <v-flex xs2>
+            <v-subheader>Nom</v-subheader>
+          </v-flex>
+          <v-flex xs10>
+            <v-text-field :value="answer.name"></v-text-field>
+          </v-flex>
+        </v-layout>
+        <v-layout row>
+          <v-flex xs2>
+            <v-subheader>Description</v-subheader>
+          </v-flex>
+          <v-flex xs10>
+            <v-text-field :value="answer.description"></v-text-field>
+          </v-flex>
+        </v-layout>
+        <v-layout row>
+          <v-flex xs2>
+            <v-subheader>Entities</v-subheader>
+          </v-flex>
+          <v-flex xs10>
+            <v-select v-model="answer.entities" chips tags :items="entities"></v-select>
+          </v-flex>
+        </v-layout>
+        <v-layout row>
+          <v-flex xs2>
+            <v-subheader>Text</v-subheader>
+          </v-flex>
+          <v-flex xs10>
+            <v-text-field multi-line :value="answer.text"
+              placeholder="texte de la réponse" :counter="380" >
+            </v-text-field>
+          </v-flex>
+        </v-layout>
+        <v-layout row>
+          <v-flex xs2>
+            <v-subheader>Children</v-subheader>
+          </v-flex>
+          <v-flex xs10>
+            <v-select v-model="answer.children" chips tags :items="otherAnswers"></v-select>
+          </v-flex>
+        </v-layout>
+
+        <v-layout row>
+          <v-flex xs2>
+            <v-subheader>Siblings</v-subheader>
+          </v-flex>
+          <v-flex xs10>
+            <v-select v-model="answer.siblings" chips tags :items="otherAnswers"></v-select>
+          </v-flex>
+        </v-layout>
+
+
+
+      </v-container>
+    </v-card-text>
+
+
+
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+               <v-btn flat>Editer</v-btn>
+               <v-btn flat color="purple">Supprimer</v-btn>
+             </v-card-actions>
+
+
           </v-card>
         </v-expansion-panel-content>
       </v-expansion-panel>
@@ -22,21 +90,32 @@
 
 <script>
 import  * as Kanzapi from '../lib/kanzapi'
-import axios from 'axios';
+import axios from 'axios'
 
 export default {
   name: 'Intent',
 
+  data() {
+    return {
+      species: this.$route.params.species,
+      intent: this.$route.params.intent,
+      answersList: {},
+      test: [],
+      entities: [],
+      otherAnswers: []
+    };
+  },
+
   created(){
     console.log("load data ", this.species, this.intent);
-    // this.intentsList = Kanzapi.getIntents(this.species, this.intent);
+    // this.answersList = Kanzapi.getIntents(this.species, this.intent);
     // TODO pb: promise ??
     const url = 'http://localhost:3000/species/'+this.species+'/intent/'+this.intent;
     console.log(url);
     axios.get(url)
       .then((response) => {
         const data = response.data;
-        this.intentsList = data;
+        this.answersList = data;
         console.log(data);
         /*
         this.answer = Object.assign({}, this.answer, data); // answer is reactive
@@ -49,13 +128,12 @@ export default {
       });
 
   },
-  data() {
-    return {
-      species: this.$route.params.species,
-      intent: this.$route.params.intent,
-      intentsList: {},
-    };
-  },
+
+  methods: {
+    callbackMethod: function (event) {
+      console.log(event);
+    },
+  }
 
 };
 </script>
