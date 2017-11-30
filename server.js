7cc4b3f29f950bad3c81cec32f29754dbbe30e1a
Fabien Saulier <fsaulier@gmail.com>
2017-11-30 12:37:20 +0100

    setup heroku env

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const serveStatic = require('serve-static')
 const app = express()
 
 app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 5000
-app.listen(port, () => console.log('Example app listening on port :'+port))
+const port = process.env.PORT || 8080
+app.listen(port, () => console.log('listening on port :'+port))
