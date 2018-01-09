f1ca96dfcbce07a1638b448bf802acd0fdeda53c
Fabien Saulier <fsaulier@gmail.com>
2018-01-09 13:55:16 +0100

    add insults

--- src/components/AnswersGeneral.vue
+++ src/components/AnswersGeneral.vue
@@ -110,7 +110,8 @@ export default {
                 {name:'goodbye', answers : []},
                 {name:'unknow', answers : []},
                 {name:'multipleMatch', answers : []},
-                {name:'sibling', answers : []}],
+                {name:'sibling', answers : []},
+                {name:'insult', answers : []}],
       items: [],
       answersName: [],
       childLabel: "",
@@ -159,34 +160,6 @@ export default {
           this.$toasted.error(errMsg, Toaster.options)
         });
     },
-
-/*
-    save: function (answer) {
-      const cleanAnswer = JSON.parse(JSON.stringify(answer));
-      axios({method:'put', url:process.env.API_URL+'/answer/', data:cleanAnswer})
-        .then((response) => {
-          this.$toasted.success(cleanAnswer.name+' enregistré', Toaster.options);
-          this.load();
-        })
-        .catch((error) => {
-          const errMsg = error.response.statusText+' - '+error.response.data.message
-          this.$toasted.error(errMsg, Toaster.options)
-        })
-    },
-
-    deleteAnswer: function(id, name){
-      if(!window.confirm("Voulez vous supprimer "+name)) return;
-      const url = process.env.API_URL+"/answer/"+id;
-      axios.delete(url)
-        .then((response) => {
-          this.$toasted.success('Réponse supprimée', Toaster.options);
-          this.load();
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    }
-    */
   }
 
 };
