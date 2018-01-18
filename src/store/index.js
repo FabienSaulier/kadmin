1ee9fb779e557c33cdf45fa7730ba208274259c0
Fabien Saulier <fsaulier@gmail.com>
2018-01-18 16:32:35 +0100

    remove  answerDialog from store

--- src/store/index.js
+++ src/store/index.js
@@ -5,14 +5,10 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    answerDialog: false,
     entities: [],
   },
 
   mutations: {
-    answerDialog() {
-      this.state.answerDialog = !this.state.answerDialog
-    },
     setEntities(state, entities) {
       const entitiesName = entities.map(x => x.name)
       this.state.entities = entitiesName
