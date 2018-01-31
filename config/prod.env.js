2977a77390eff04227d30af4299b8e709da8e21b
Fabien Saulier <fsaulier@gmail.com>
2018-01-31 02:22:50 +0100

    allow disable login

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_URL: '"https://kanzi.herokuapp.com"'
+  API_URL: '"https://kanzi.herokuapp.com"',
+  BASE_URL: '"https://kanziadmin.herokuapp.com"',
+  DISABLED_LOGIN: '"false"',
 }
