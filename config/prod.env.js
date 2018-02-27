0aa2b6160ae1f6ee72cf9d57e410c12b248851f1
Fabien Saulier <fsaulier@gmail.com>
2018-02-27 13:20:32 +0100

    export api url

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,7 +1,5 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_URL: '"https://kanzi.herokuapp.com"',
-  BASE_URL: '"https://kanziadmin.herokuapp.com"',
   DISABLED_LOGIN: false,
 }
