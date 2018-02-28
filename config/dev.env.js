1cb08409d9148e2bdd8311a60c5f9338f4bf1ac3
Fabien Saulier <fsaulier@gmail.com>
2018-02-28 19:07:45 +0100

    conf env

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,7 +4,7 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_URL: '"https://feeleat.herokuapp.com/"',
-  BASE_URL: '"https://feeleatadmin.herokuapp.com/"',
+  API_URL: '"https://feeleat.herokuapp.com"',
+  BASE_URL: '"https://feeleatadmin.herokuapp.com"',
   DISABLED_LOGIN: false,
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,7 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_URL: '"https://feeleat.herokuapp.com/"',
-  BASE_URL: '"https://feeleatadmin.herokuapp.com/"',
+  API_URL: '"https://feeleat.herokuapp.com"',
+  BASE_URL: '"https://feeleatadmin.herokuapp.com"',
   DISABLED_LOGIN: false,
 }
